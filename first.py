572dffa6e805777aa0191e0212e0801a6513e4ad
Overchenko-Egor <egoverchenko@mail.ru>
2023-04-24 03:08:15 +0700

    ОНО РАБОТАЛООО......

--- first.py
+++ first.py
@@ -13,9 +13,17 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
 	r = requests.get("https://auto.drom.ru")
-	soup = bs(r.text, 'htmm.parser')
-	models_car = soup.find_all('a', class_='css-1q66we5 e4ojbx43')
-	await message.reply(models_car) # отвечает на сообщение
+	soup = bs(r.text, 'html.parser')
+	models_car_c = "."
+	models_car = soup.find_all('div', class_='css-lahb1a e4ojbx44')
+	for a in models_car:
+		if len(a) > 0:
+			models_car_c += a.find('span', 'css-1kb7l9z e162wx9x0').text
+	#models_car += soup.find_all('div', class_='css-pvjszw')
+	#models_car += soup.find_all('a', class_='css-1q66we5 e4ojbx43')
+	#clear_m_c = [c.text for c in models_car]
+	await message.answer(models_car_c)
+	print (models_car_c)
 	
 
 @dp.message_handler(commands=['find'])
